393b1eda4188bdd4310a1d19026e857d301d7696
Arthur <usoprotumblr@gmail.com>
2023-02-08 22:41:36 -0300

    update script.js
    added copy hex button

--- script.js
+++ script.js
@@ -18,15 +18,20 @@ let colorsArr = [
 ]
 
 const btnColor = document.querySelector('#btn-color')
-const hexColor = document.querySelector('#hex-color')
+const btnHex = document.querySelector('#btn-hex')
 const colorName = document.querySelector('#color')
 const hexCode = document.querySelector('#hex')
 
 btnColor.addEventListener('click', changeColor)
+btnHex.addEventListener('click', copyToClipboard)
 
 function changeColor(){
     let randomNum = Math.floor(Math.random() * colorsArr.length) // Get a random array with a color and its HEX code.
     document.body.style.backgroundColor = colorsArr[randomNum][1]
     colorName.textContent = `${colorsArr[randomNum][0]}` // Changes the color name.
     hexCode.textContent = `${colorsArr[randomNum][1]}` // Changes the HEX code.
+}
+
+function copyToClipboard(){
+    navigator.clipboard.writeText(hexCode.textContent)
 }
